3b89e0677482702c6d743df0b9a773b77a8c091c
Yuri <Yuri@Yuri-PC>
2018-01-23 09:21:52 +0900

    team/yr : 퇴근푸시 180122

---
+++ nadrii/src/main/java/com/yagn/nadrii/service/trip/impl/TripServiceImpl.java
@@ -0,0 +1,51 @@
+package com.yagn.nadrii.service.trip.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.yagn.nadrii.service.trip.TourAPlUrlManage;
+import com.yagn.nadrii.service.trip.TourApiDomain;
+import com.yagn.nadrii.service.trip.TripDao;
+import com.yagn.nadrii.service.trip.TripService;
+
+@Service("tripServiceImpl")
+public class TripServiceImpl implements TripService {
+	
+	@Autowired
+	@Qualifier("tripDaoImpl")
+	private TripDao tripDao;
+	
+	
+	
+	public TripServiceImpl() {
+		System.out.println(this.getClass());
+		
+	}
+
+
+
+	@Override
+	public Map listTrip(TourAPlUrlManage tourAPlUrlManage) throws Exception{
+		Map map = new HashMap();
+		
+		List list = tripDao.listTrip(tourAPlUrlManage);
+		map.put("list", list);
+		return map;
+	}
+
+
+
+	@Override
+	public TourApiDomain getTrip() throws Exception {
+		
+		return null;
+	}
+	
+	
+	
+}
---
+++ nadrii/src/main/java/com/yagn/nadrii/web/trip/TripRestController.java
@@ -0,0 +1,58 @@
+package com.yagn.nadrii.web.trip;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yagn.nadrii.service.trip.TourAPlUrlManage;
+import com.yagn.nadrii.service.trip.TripService;
+
+
+@RestController
+@RequestMapping("/trip/*")
+public class TripRestController {
+	
+	@Autowired
+	@Qualifier("tripServiceImpl")
+	private TripService tripService;
+	
+		
+	public TripRestController() {
+		System.out.println(this.getClass());
+	}
+
+
+	@RequestMapping(value="json/listMuseum/{pageNo}")
+	public Map listMuseum(@PathVariable("pageNo")int pageNo) throws Exception{
+		TourAPlUrlManage tourAPlUrlManage = new TourAPlUrlManage();
+		tourAPlUrlManage.urlClean();
+		tourAPlUrlManage.setContentTypeId("14");
+		tourAPlUrlManage.setCat1("A02");
+		tourAPlUrlManage.setCat2("A0206");
+		tourAPlUrlManage.setCat3("A02060100");
+		tourAPlUrlManage.setPageNo(pageNo);
+		
+		Map map = new HashMap();
+		
+		System.out.println("/trip/json/listMuseum");
+		
+		Map tripMap = tripService.listTrip(tourAPlUrlManage); 
+		map.put("trip", tripMap.get("list"));
+		map.put("pageNo", pageNo);		
+		System.out.println(pageNo);
+		
+		
+		return map;
+	}
+	
+	
+	
+
+}
