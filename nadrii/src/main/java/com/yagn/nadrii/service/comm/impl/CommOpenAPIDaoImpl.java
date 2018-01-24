40d2ff321f3bd5694702b609aeb9fb8db7ebdb7c
Yuri <Yuri@Yuri-PC>
2018-01-24 09:46:37 +0900

    workspce 이동용

--- nadrii/src/main/java/com/yagn/nadrii/service/comm/impl/CommOpenAPIDaoImpl.java
+++ nadrii/src/main/java/com/yagn/nadrii/service/comm/impl/CommOpenAPIDaoImpl.java
@@ -1,14 +1,26 @@
 package com.yagn.nadrii.service.comm.impl;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.stereotype.Repository;
 
+import com.google.cloud.vision.v1.AnnotateImageRequest;
 import com.yagn.nadrii.service.comm.CommOpenAPIDao;
 
 @Repository("commOpenAPIDaoImpl")
 public class CommOpenAPIDaoImpl implements CommOpenAPIDao {
 
 	@Override
-	public String getImageResult(String filename) {
+	public String getImageResult(String filename){
+		HttpClient httpClient = new DefaultHttpClient();
+		
+		String url="https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDn7y8YBw6t2udCzfFw5HiuiMJNTyZlIk8";
+		HttpPost httpPost = new HttpPost();
+		httpPost.setHeader("Accept", "application/json");
+		httpPost.setHeader("Content-Type", "application/json");
+		
+		
 		return null;
 	}
 	
