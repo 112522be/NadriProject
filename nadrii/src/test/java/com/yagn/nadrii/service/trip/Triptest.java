594ddf38aadd9b12bd4003f7bd66cf14745f2a3d
user <user@DESKTOP-5UMDQF4>
2018-01-26 10:17:22 +0900

    team/jm trip Mapper 단위 테스트 중 180126

--- nadrii/src/test/java/com/yagn/nadrii/service/trip/Triptest.java
+++ nadrii/src/test/java/com/yagn/nadrii/service/trip/Triptest.java
@@ -1,21 +1,42 @@
 package com.yagn.nadrii.service.trip;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.yagn.nadrii.common.Search;
 import com.yagn.nadrii.service.comm.impl.CommDaoImpl;
 import com.yagn.nadrii.service.domain.Community;
 import com.yagn.nadrii.service.domain.Trip;
 import com.yagn.nadrii.service.trip.impl.TripDaoImpl;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@ContextConfiguration	(locations = {	"classpath:config/context-common.xml",
+		"classpath:config/context-aspect.xml",
+		"classpath:config/context-mybatis.xml",
+		"classpath:config/context-transaction.xml" })
+
+
 public class Triptest {
 	
 	private Trip trip;
 	
-	private TripDaoImpl tripDaoImpl;
+	@Autowired
+	@Qualifier("tripDaoImpla")
+	private TripDao tripDao;
 	
-	@Test
+	//private TripDaoImpl tripDaoImpl;
+	
+	//@Test
 	public void addTripTest() throws Exception {
-		tripDaoImpl = new TripDaoImpl();
+		
 		trip = new Trip();
 		trip.setLng("test경도");
 		trip.setLat("test위도");
@@ -28,18 +49,41 @@ public class Triptest {
 		trip.setTripName("test여행지");
 		
 		
-		tripDaoImpl.addTrip(trip);
+		tripDao.addTrip(trip);
 		
 		
 		
 	}
 	
 	//@Test
-	public void testtest() throws Exception{
-		CommDaoImpl commDaoImpl = new CommDaoImpl();
-	
-		System.out.println(commDaoImpl.getComm(20001));
-				
+	public void getTripTest() throws Exception{
+		trip =tripDao.getTrip(20000);
+		System.out.println(trip);
+		
 	}
+	
+	//@Test
+	public void listTrip() throws Exception{
+		Search search = new Search();
+		search.setSearchCondition("1");
+		search.setSearchKeyword("%주소지%");
+		search.setCurrentPage(1);
+		search.setPageSize(3);
+		
+		List list = tripDao.listTrip(search);
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+			
+		}
+		
+		
+	}
+	
+	@Test
+	public void updateViewCount() throws Exception{
+		
+		
+	}
+	
 
 }
