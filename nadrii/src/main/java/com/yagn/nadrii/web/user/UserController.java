f5a67ee2306dc2691f251297e1f729b78f1d3176
juhyun <juhyun@juhyunkim>
2018-01-29 13:31:47 +0900

    team/js : EmailSender추가, context-common, UserConroller, User 		메일인증 구현위해 추가 / 18.01.29
    더 구현해야 됨...

--- nadrii/src/main/java/com/yagn/nadrii/web/user/UserController.java
+++ nadrii/src/main/java/com/yagn/nadrii/web/user/UserController.java
@@ -1,6 +1,5 @@
 package com.yagn.nadrii.web.user;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -19,15 +18,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.yagn.nadrii.service.domain.User;
-import com.yagn.nadrii.service.user.UserDao;
 import com.yagn.nadrii.service.user.UserService;
 
 @Controller
@@ -189,6 +185,8 @@ public class UserController extends SupportController {
 //		}
 //		return buffer.toString();
 //	}
+	
+	
 	
 	/**
 	 *  login ∆‰¿Ã¡ˆ
