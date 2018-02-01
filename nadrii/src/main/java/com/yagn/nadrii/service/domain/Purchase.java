5ad7dc299667d42db0b6cb6087a6171b92e4f67d
bitcamp <bitcamp@bit>
2018-02-01 16:57:54 +0900

    team/sh : git error 해결을 위한 백업!!!!!!
    - 180201

--- nadrii/src/main/java/com/yagn/nadrii/service/domain/Purchase.java
+++ nadrii/src/main/java/com/yagn/nadrii/service/domain/Purchase.java
@@ -8,141 +8,107 @@ public class Purchase {
 	private int contentId;			
 	private int contentTypeId;		
 
-	private int ticketNo;			
-	private String buyer;				
+	private int postNo;			
 	private String ticketTitle;		
-	private String imageFile;		
+	private String ticketImage;		
 	private Date regDate;			
 	private String bookingDate;		
-	private String cancelDeadline;	
-
+	private String cancelDate;	
 	private String ticketPriceAll;
-	
 	private String flag; 			
-	
 
 	public Purchase() {
 		
 	}
 
-
 	public int getContentId() {
 		return contentId;
 	}
-
 
 	public void setContentId(int contentId) {
 		this.contentId = contentId;
 	}
 
-
 	public int getContentTypeId() {
 		return contentTypeId;
 	}
-
 
 	public void setContentTypeId(int contentTypeId) {
 		this.contentTypeId = contentTypeId;
 	}
 
-
-	public int getTicketNo() {
-		return ticketNo;
+	public int getPostNo() {
+		return postNo;
 	}
 
-	public void setTicketNo(int ticketNo) {
-		this.ticketNo = ticketNo;
+	public void setPostNo(int postNo) {
+		this.postNo = postNo;
 	}
-/*
-	public User getBuyer() {
-		return buyer;
-	}
-
-
-	public void setBuyer(User buyer) {
-		this.buyer = buyer;
-	}
-//*/
 
 	public String getTicketTitle() {
 		return ticketTitle;
 	}
 
-
 	public void setTicketTitle(String ticketTitle) {
 		this.ticketTitle = ticketTitle;
 	}
 
-
-	public String getImageFile() {
-		return imageFile;
+	public String getTicketImage() {
+		return ticketImage;
 	}
 
-
-	public void setImageFile(String imageFile) {
-		this.imageFile = imageFile;
+	public void setTicketImage(String ticketImage) {
+		this.ticketImage = ticketImage;
 	}
-
 
 	public Date getRegDate() {
 		return regDate;
 	}
 
-
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
 	}
-
 
 	public String getBookingDate() {
 		return bookingDate;
 	}
 
-
 	public void setBookingDate(String bookingDate) {
 		this.bookingDate = bookingDate;
 	}
 
-
-	public String getCancelDeadline() {
-		return cancelDeadline;
+	public String getCancelDate() {
+		return cancelDate;
 	}
 
-
-	public void setCancelDeadline(String cancelDeadline) {
-		this.cancelDeadline = cancelDeadline;
+	public void setCancelDate(String cancelDate) {
+		this.cancelDate = cancelDate;
 	}
-
 
 	public String getTicketPriceAll() {
 		return ticketPriceAll;
 	}
 
-
 	public void setTicketPriceAll(String ticketPriceAll) {
 		this.ticketPriceAll = ticketPriceAll;
 	}
-
 
 	public String getFlag() {
 		return flag;
 	}
 
-
 	public void setFlag(String flag) {
 		this.flag = flag;
 	}
 
-
 	@Override
 	public String toString() {
-		return "Purchase [contentId=" + contentId + ", contentTypeId=" + contentTypeId + ", ticketNo=" + ticketNo
-				+ ", buyer=" + buyer + ", ticketTitle=" + ticketTitle + ", imageFile=" + imageFile + ", regDate="
-				+ regDate + ", bookingDate=" + bookingDate + ", cancelDeadline=" + cancelDeadline + ", ticketPriceAll="
-				+ ticketPriceAll + ", flag=" + flag + "]";
+		return "Purchase [contentId=" + contentId + ", contentTypeId=" + contentTypeId + ", postNo=" + postNo
+				+ ", ticketTitle=" + ticketTitle + ", ticketImage=" + ticketImage + ", regDate=" + regDate
+				+ ", bookingDate=" + bookingDate + ", cancelDate=" + cancelDate + ", ticketPriceAll=" + ticketPriceAll
+				+ ", flag=" + flag + "]";
 	}
 
-	
 	
 	
 	
