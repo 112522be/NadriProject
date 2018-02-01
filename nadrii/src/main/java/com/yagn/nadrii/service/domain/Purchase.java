c07422821d3ce3884f1ab5100ea15466971d8429
bitcamp <bitcamp@bit>
2018-02-01 16:33:42 +0900

    team/sh : sql mapper 구현중
    - 180201
    - yagun 서버 테이블 수정 불가로 개인 오라클 DB에 테이블 생성후 TEST

--- nadrii/src/main/java/com/yagn/nadrii/service/domain/Purchase.java
+++ nadrii/src/main/java/com/yagn/nadrii/service/domain/Purchase.java
@@ -5,27 +5,46 @@ import java.sql.Date;
 public class Purchase {
 	
 	/// Field
+	private int contentId;			
+	private int contentTypeId;		
+
 	private int ticketNo;			
-	private User buyer;				
+	private String buyer;				
 	private String ticketTitle;		
 	private String imageFile;		
 	private Date regDate;			
 	private String bookingDate;		
-	private String lat; 			
-	private String lng; 			
 	private String cancelDeadline;	
+
+	private String ticketPriceAll;
 	
-	private int price;				
-	private int ticketType;			
-	private int ticketCount;		
-	
-	private int contentId;			
-	private int contentTypeId;		
 	private String flag; 			
 	
+
 	public Purchase() {
 		
 	}
+
+
+	public int getContentId() {
+		return contentId;
+	}
+
+
+	public void setContentId(int contentId) {
+		this.contentId = contentId;
+	}
+
+
+	public int getContentTypeId() {
+		return contentTypeId;
+	}
+
+
+	public void setContentTypeId(int contentTypeId) {
+		this.contentTypeId = contentTypeId;
+	}
+
 
 	public int getTicketNo() {
 		return ticketNo;
@@ -34,127 +53,96 @@ public class Purchase {
 	public void setTicketNo(int ticketNo) {
 		this.ticketNo = ticketNo;
 	}
-
+/*
 	public User getBuyer() {
 		return buyer;
 	}
 
+
 	public void setBuyer(User buyer) {
 		this.buyer = buyer;
 	}
+//*/
 
 	public String getTicketTitle() {
 		return ticketTitle;
 	}
 
+
 	public void setTicketTitle(String ticketTitle) {
 		this.ticketTitle = ticketTitle;
 	}
+
 
 	public String getImageFile() {
 		return imageFile;
 	}
 
+
 	public void setImageFile(String imageFile) {
 		this.imageFile = imageFile;
 	}
+
 
 	public Date getRegDate() {
 		return regDate;
 	}
 
+
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
 	}
+
 
 	public String getBookingDate() {
 		return bookingDate;
 	}
 
+
 	public void setBookingDate(String bookingDate) {
 		this.bookingDate = bookingDate;
 	}
 
-	public String getLat() {
-		return lat;
-	}
-
-	public void setLat(String lat) {
-		this.lat = lat;
-	}
-
-	public String getLng() {
-		return lng;
-	}
-
-	public void setLng(String lng) {
-		this.lng = lng;
-	}
 
 	public String getCancelDeadline() {
 		return cancelDeadline;
 	}
 
+
 	public void setCancelDeadline(String cancelDeadline) {
 		this.cancelDeadline = cancelDeadline;
 	}
 
-	public int getPrice() {
-		return price;
+
+	public String getTicketPriceAll() {
+		return ticketPriceAll;
 	}
 
-	public void setPrice(int price) {
-		this.price = price;
+
+	public void setTicketPriceAll(String ticketPriceAll) {
+		this.ticketPriceAll = ticketPriceAll;
 	}
 
-	public int getTicketType() {
-		return ticketType;
-	}
-
-	public void setTicketType(int ticketType) {
-		this.ticketType = ticketType;
-	}
-
-	public int getTicketCount() {
-		return ticketCount;
-	}
-
-	public void setTicketCount(int ticketCount) {
-		this.ticketCount = ticketCount;
-	}
-
-	public int getContentId() {
-		return contentId;
-	}
-
-	public void setContentId(int contentId) {
-		this.contentId = contentId;
-	}
-
-	public int getContentTypeId() {
-		return contentTypeId;
-	}
-
-	public void setContentTypeId(int contentTypeId) {
-		this.contentTypeId = contentTypeId;
-	}
 
 	public String getFlag() {
 		return flag;
 	}
 
+
 	public void setFlag(String flag) {
 		this.flag = flag;
 	}
 
+
 	@Override
 	public String toString() {
-		return "Purchase [ticketNo=" + ticketNo + ", buyer=" + buyer + ", ticketTitle=" + ticketTitle + ", imageFile="
-				+ imageFile + ", regDate=" + regDate + ", bookingDate=" + bookingDate + ", lat=" + lat + ", lng=" + lng
-				+ ", cancelDeadline=" + cancelDeadline + ", price=" + price + ", ticketType=" + ticketType
-				+ ", ticketCount=" + ticketCount + ", contentId=" + contentId + ", contentTypeId=" + contentTypeId
-				+ ", flag=" + flag + "]";
+		return "Purchase [contentId=" + contentId + ", contentTypeId=" + contentTypeId + ", ticketNo=" + ticketNo
+				+ ", buyer=" + buyer + ", ticketTitle=" + ticketTitle + ", imageFile=" + imageFile + ", regDate="
+				+ regDate + ", bookingDate=" + bookingDate + ", cancelDeadline=" + cancelDeadline + ", ticketPriceAll="
+				+ ticketPriceAll + ", flag=" + flag + "]";
 	}
+
+	
 	
 	
 	
