08d2637fc057b357c2d3faeea215358ee6972beb
user <user@DESKTOP-5UMDQF4>
2018-02-11 15:12:05 +0900

    team/jm Toast기능 및 메시지 수정

--- nadrii/src/main/java/com/yagn/nadrii/web/message/MessageController.java
+++ nadrii/src/main/java/com/yagn/nadrii/web/message/MessageController.java
@@ -52,10 +52,11 @@ public class MessageController {
 	
 
 	@RequestMapping(value="addMessage", method=RequestMethod.POST)
-	public String addMessage(@ModelAttribute Message message,Map map,@RequestParam("receiverId") String receiverId)throws Exception {
+	public String addMessage(@ModelAttribute Message message,Map map)throws Exception {
 		System.out.println(this.getClass()+"/message/addMessage/");
-		System.out.println("POST"+receiverId);
+		
 		System.out.println(message);
+		
 		
 		messageService.addMessage(message);
 		
@@ -90,6 +91,7 @@ public class MessageController {
 		
 		List<Message> list = messageService.listMessage(user.getUserId());
 		
+		/*
 		for (int i = 0; i < list.size(); i++) {
 			
 			if((list.get(i).getText()).length() >10) {
@@ -98,14 +100,14 @@ public class MessageController {
 				list.get(i).setText(text);			
 			}
 		}
-		
+		*/
 		//System.out.println(list.get(0));
 		map.put("list", list);
 		
 		return "forward:/message/listMessage.jsp";
 	}
 	
-	//����Ʈ�� �̵�??
+
 	private void updateReadFlag(int messageNo) throws Exception{
 		System.out.println(this.getClass()+"  updateReadFlag");
 		
